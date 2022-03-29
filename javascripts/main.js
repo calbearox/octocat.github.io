8ebabae671230b1281f688a144d9203f3fbbae31
calbearox <99705133+calbearox@users.noreply.github.com>
2022-03-28 18:56:57 -0700

    Update main.js
    fn main() {
        println!("Hello, world!");
    }

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
 rootProject.name ​=​ ​"​io/.github.https://maldit-dot.maldito-dot​"
+.
++-- node_modules
+|  `-- packages/a -> ../packages/a
++-- package-lock.json
++-- package.json
+`-- packages
+   +-- a
+   |   `-- package.json
